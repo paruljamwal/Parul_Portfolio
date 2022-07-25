84da64856cca0018661e478de3b702e480ae1513
paruljamwal <paruljamwal2000@gmail.com>
2022-07-25 23:05:26 +0530

    add redux  react project

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,23 +4,31 @@ import IMG2 from "../../assets/freshly.jpg";
 import IMG3 from "../../assets/Zappos.jpg";
 import IMG4 from '../../assets/Lifestyle.jpg'
 import final from '../../assets/final.png';
+import final1 from '../../assets/final1.png';
 var data=[
   {
     id:1,
+    image:final1,
+    title:'Everhour is a time tracker website that helps small and mid-size companies  to  organize their working hours.',
+    github:'https://github.com/paruljamwal/viable-argument-168.git',
+    demo:'https://everhour-clone.netlify.app/'
+  },
+  {
+    id:2,
     image:IMG4,
     title:'LifeStyleStores is a Product based website',
     github:'https://github.com/paruljamwal/LifeStyleStores.git',
     demo:'https://lifestyle-final-main.vercel.app/'
   },
   {
-    id:2,
+    id:3,
     image:final,
     title:'OverStock is a American product based organisation where you can buy home decor, furniture etc.',
     github:'https://github.com/paruljamwal/OverStock.git',
     demo:'https://whimsical-hamster-e7693c.netlify.app'
   },
   {
-    id:3,
+    id:4,
     image:IMG1,
     title:'BEST BUY is a Electronic products based web.',
     github:'https://github.com/paruljamwal/Zappos-Clone.git',
@@ -28,14 +36,14 @@ var data=[
   },
 
   {
-    id:4,
+    id:5,
     image:IMG2,
     title:'FRESHLY is an organization who delivers food in different areas.',
     github:'https://github.com/paruljamwal/Fressely.git',
     demo:'https://sid283.github.io/Ecommerce_freshly_clone/landing_page/homepage.html'
   },
   {
-    id:5,
+    id:6,
     image:IMG3,
     title:'Ecommerce web. provides clothes and Major know for Footwear.',
     github:'https://github.com/paruljamwal/Zappos.git',
