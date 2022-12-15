d5b3cad7a96284085e64cd7e7162c807c54c920a
paruljamwal <paruljamwal2000@gmail.com>
2022-12-15 23:01:04 +0530

    some changes in projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -10,6 +10,7 @@ var data=[
   {
     id:1,
     image:final1,
+    name:"EverHour",
     title:'Everhour is a time tracker website that helps small and mid-size companies  to  organize their working hours.',
     github:'https://github.com/paruljamwal/viable-argument-168.git',
     demo:'https://idyllic-semifreddo-852580.netlify.app'
@@ -18,21 +19,17 @@ var data=[
   {
     id:2,
     image:final,
-    title:'OverStock is a American product based organisation where you can buy home decor, furniture etc.',
+    name:"OverStock",
+    title:'OverStock is a American product based organisation where you can buy home decor, furniture, indoor-outdoor tents etc.',
     github:'https://github.com/paruljamwal/OverStock.git',
-    demo:'https://over-bz1zdp857-paruljamwal.vercel.app'
+    demo:'https://endearing-malabi-e92ee0.netlify.app/'
   },
-  {
-    id:3,
-    image:IMG4,
-    title:'LifeStyleStores is a Product based website',
-    github:'https://github.com/paruljamwal/LifeStyleStores.git',
-    demo:'https://lifestyle-final-main.vercel.app/'
-  },
+
   {
     id:4,
     image:IMG1,
-    title:'BEST BUY is a Electronic products based web.',
+    name:"BEST BUY",
+    title:'The company sells a wide variety of items, including mobile phones, and video games, home appliances.',
     github:'https://github.com/paruljamwal/BestBuy_Clone',
     demo:'https://roaring-cassata-209e07.netlify.app/'
   },
@@ -40,14 +37,16 @@ var data=[
   {
     id:5,
     image:IMG2,
-    title:'FRESHLY is an organization who delivers food in different areas.',
+    name:"FRESHLY",
+    title:'Freshly clone is a pepared meal delivery website which not only aims at just delivering food but also focuses on nutrition and calories.',
     github:'https://github.com/paruljamwal/Fressely.git',
     demo:'https://sid283.github.io/Ecommerce_freshly_clone/landing_page/homepage.html'
   },
   {
     id:6,
     image:IMG3,
-    title:'Ecommerce web. provides clothes and Major know for Footwear.',
+    name:"Zappos",
+    title:'Ecommerce web. provides clothes and Major know for Footwear. At Zappos.com, our purpose is simple: to live and deliver WOW and best service, experience.',
     github:'https://github.com/paruljamwal/Zappo_Tappo_Website_USA.git',
     demo:'https://cozy-mandazi-5494a7.netlify.app/'
   }
@@ -55,9 +54,18 @@ var data=[
   {
     id:7,
     image:IMG5,
+    name:"Snow glove",
     title:'Snow glove: add any image and gift to your loved one.',
     github:'https://github.com/paruljamwal/snow_globe',
     demo:'https://famous-mousse-3232bc.netlify.app/'
+  },
+  {
+    id:3,
+    image:IMG4,
+    name:"LifestyleStores",
+    title:'LifestyleStores is a retail Fashion Brand. Which has all clothing in varies categories and has a wide range of fashion products',
+    github:'https://github.com/paruljamwal/LifeStyleStores.git',
+    demo:'https://lifestyle-final-main.vercel.app/'
   }
  
 ]
@@ -70,12 +78,13 @@ export const Portfolio = () => {
 
       <div className="container portfolio__container">
        {
-         data.map(({id,image,title,github,demo})=>{
+         data.map(({id,image,name,title,github,demo})=>{
            return (
             <article key={id} className="portfolio__item">
             <div className="Portfolio__item-image">
               <img src={image} alt="image" />
             </div>
+            <h2 style={{textAlign:"center"}} >{name}</h2>
             <h3>{title}</h3>
             <div className="portfolio__item-cta">
             <a href={github} className="btn github" target="_blank">
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,11 @@
-import CV from "../../assets/cv.pdf";
 
 export const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV}download className="btn">Download Resume</a>
+        <a href={"https://drive.google.com/file/d/1tIrRqGqAqVi5LqxeAT05bXqILyhBTD0z/view?usp=share_link"} className="btn btn-primary github" target="_blank">
+            Resume
+            </a>
+
       <a href="#contact" className="btn btn-primary">Let's Talk</a>
     </div>
   );
